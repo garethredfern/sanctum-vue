3492ffa07602e768ff02820490e676acd9b3f060
Gareth Redfern <gareth@garethredfern.com>
2020-01-12 07:47:17 +0000

    ✨add a logout button and method

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,7 +4,12 @@
       <h1 class="uppercase">Dashboard</h1>
     </header>
     <div class="p-5">
-      <p class="text-blue-500">Welcome to your dashboard...</p>
+      <p class="text-blue-500 mb-2">Welcome to your dashboard...</p>
+      <button
+        type="button"
+        class="bg-blue-400 hover:bg-blue-500 trans text-white rounded px-4 py-1 text-sm focus:outline-none focus:shadow-outline"
+        @click="logout"
+      >Logout</button>
     </div>
   </article>
 </template>
@@ -16,6 +21,11 @@ export default {
   name: "Dashboard",
   computed: {
     ...mapGetters("auth", ["authUser", "loggedIn"])
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("auth/logout");
+    }
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ module.exports = {
     sourceMap: true
   },
   devServer: {
+    // proxy to use http://localhost:8080
+    // this gets round CORS issues.
     proxy: "http://airlock-example.test"
   }
 };
