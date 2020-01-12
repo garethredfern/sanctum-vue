266d6954a4fd53b5b448f37a7adf4909998039b8
Gareth Redfern <gareth@garethredfern.com>
2020-01-12 00:27:31 +0000

    ♻️check for token not auth user in local storage

--- src/router.js
+++ src/router.js
@@ -36,8 +36,8 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  const loggedIn = localStorage.getItem("authUser");
-  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+  const token = localStorage.getItem("token");
+  if (to.matched.some(record => record.meta.requiresAuth) && !token) {
     next({ path: "/login", query: { redirect: to.fullPath } });
   } else {
     next(); // make sure to always call next()!
